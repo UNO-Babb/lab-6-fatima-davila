efa17ea07b3598f84a93c2103bea7235965dc21b
fatima-davila <fdavila@unomaha.edu>
2025-02-28 17:16:17 +0000

    word count

--- WordCount.py
+++ WordCount.py
@@ -4,10 +4,23 @@
 #Assignment:
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
+  textFile = open("fish.txt", 'r')
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
+  charCount = 0
   
   for line in textFile:
-    print(line)
+    lineCount = lineCount + 1
+    word = line.split()
+    for w in word:
+      wordCount = wordCount +1
+      charCount += len(line)
+    #print(line)
+  
+  print("lines:", lineCount)
+  print("Word:", wordCount)
+  print("character:", charCount)
   
 
 if __name__ == '__main__':
