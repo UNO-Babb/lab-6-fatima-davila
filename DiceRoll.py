b79151fd12affbd4f1280f6ae2bf8e8f5dbe291e
fatima-davila <fdavila@unomaha.edu>
2025-02-28 16:18:01 +0000

    dice

--- DiceRoll.py
+++ DiceRoll.py
@@ -8,11 +8,18 @@ def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
+  for r in range (100):
+    dice1 = random.randint(1,6)
+    rolls[dice1 - 1] = rolls[dice1 - 1] + 1
   #find the sum total of the two dice
   
   #print statictics for dice rolls
+  dice = 1
+  for count in rolls:
+    print(dice,":", count)
+    dice = dice + 1
 
+  print(rolls)
 
 if __name__ == '__main__':
   main()
